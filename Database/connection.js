c06c882b0afa75d7be54538bf9b7825e1fbd0e30
Keanu Hasty <keanu.hasty@gmail.com>
2021-08-15 12:13:22 -0600

    abstracted pool connection/query/errors to db connection for reuse

--- Database/connection.js
+++ Database/connection.js
@@ -16,4 +16,46 @@ pool.on('error', (err, client) => {
   process.exit(-1);
 });
 
-module.exports = pool;
+const makeQuery = async (query, title) => {
+  try {
+    console.log(`---------- ${title} ---------`);
+    console.log('1.) Attempting to connect to Pool');
+
+    const beforePoolConnect = new Date();
+    const client = await pool.connect();
+    const afterPoolConnect = new Date();
+
+    console.log('2.) Successfully connected to Pool, client checked out. TIME TO CONNECT: ', afterPoolConnect - beforePoolConnect, ' ms');
+    try {
+      // some sort of helper to verify the values are valid
+      console.log(`3.) Attempting to query DB for ${title}`);
+
+      const beforeQuery = new Date();
+      const queryRes = await client.query(query);
+      const afterQuery = new Date();
+
+      console.log(`4.) Successful query made for ${title}. TIME TO QUERY: `, afterQuery - beforeQuery, ' ms');
+      return queryRes.rows;
+    } catch (err) {
+      console.log('4.) Error queryind data: ', err);
+
+      throw new Error('Error querying DB', { cause: err });
+    } finally {
+      console.log('5.) returning client to pool');
+
+      client.release();
+      console.log('6.) client returned to pool');
+    }
+  } catch (err) {
+    // handle error from inner try/catch block
+    if (err.message === 'Error querying DB') {
+      throw err;
+    }
+
+    // throw custom error if connection to pool fails
+    console.log('3.) Error connecting to pool: ', err);
+    throw new Error('DB Pool Connection Error', { cause: err });
+  }
+};
+
+module.exports = makeQuery;
