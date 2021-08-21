c69c8cd9700362d4584bfd55af038e5186a47e58
Keanu Hasty <keanu.hasty@gmail.com>
2021-08-21 09:29:57 -0600

    updated ip's for new day of tests

--- nginxConnect.sh
+++ nginxConnect.sh
@@ -1,2 +1,2 @@
-scp -i HR-sdc.pem ./init.sh ec2-user@ec2-18-218-131-214.us-east-2.compute.amazonaws.com:~/init.sh
-ssh -i HR-sdc.pem ec2-user@ec2-18-218-131-214.us-east-2.compute.amazonaws.com
+scp -i HR-sdc.pem ./init.sh ec2-user@ec2-18-118-13-87.us-east-2.compute.amazonaws.com:~/init.sh
+ssh -i HR-sdc.pem ec2-user@ec2-18-118-13-87.us-east-2.compute.amazonaws.com
--- nodeConnect.sh
+++ nodeConnect.sh
@@ -1,2 +1,2 @@
-scp -i HR-sdc.pem ./init.sh ec2-user@ec2-18-118-129-65.us-east-2.compute.amazonaws.com:~/init.sh
-ssh -i HR-sdc.pem ec2-user@ec2-18-118-129-65.us-east-2.compute.amazonaws.com
+scp -i HR-sdc.pem ./init.sh ec2-user@ec2-18-219-50-132.us-east-2.compute.amazonaws.com:~/init.sh
+ssh -i HR-sdc.pem ec2-user@ec2-18-219-50-132.us-east-2.compute.amazonaws.com
